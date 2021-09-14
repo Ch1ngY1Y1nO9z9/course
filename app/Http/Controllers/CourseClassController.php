80650bb496783b3222a71ee4cdac1f59667a8fb5
l123050891 <l123050891@gmail.com>
2021-09-14 14:43:21 +0800

    update mail server test

--- app/Http/Controllers/CourseClassController.php
+++ app/Http/Controllers/CourseClassController.php
@@ -8,13 +8,16 @@ use App\ClassAnnounces;
 use Illuminate\Http\Request;
 use App\Jobs\CheckCoursesStatus;
 use App\Jobs\CheckAnnounceStatus;
+use App\Mail\Testmail;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 use Maatwebsite\Excel\Facades\Excel;
 
 class CourseClassController extends Controller
 {
     public function annocunce()
     {
+        Mail::to('l123050891@gmail.com')->send(new Testmail());
         dispatch(new CheckAnnounceStatus());
     }
 
