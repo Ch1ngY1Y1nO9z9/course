1f7d5b3eeebaad5ab00e7c1e3dd7cb11af819bac
l123050891 <l123050891@gmail.com>
2021-10-18 13:50:22 +0800

    update kernel test

--- app/Console/Commands/checkClassStatus.php
+++ app/Console/Commands/checkClassStatus.php
@@ -13,7 +13,7 @@ class checkClassStatus extends Command
      *
      * @var string
      */
-    protected $signature = 'command:checkClassStatus';
+    protected $signature = 'checkClassStatus';
 
     /**
      * The console command description.
@@ -39,30 +39,31 @@ class checkClassStatus extends Command
      */
     public function handle()
     {
-        // 每天午夜12點執行一次將所有課程狀態做一次調整
+        // // 每天午夜12點執行一次將所有課程狀態做一次調整
         
-        $date = strtotime(date('m/d/Y', time()));
-        $passed_course = Courses::where('status', '已通過')->get();
+        // $date = strtotime(date('m/d/Y', time()));
+        // $passed_course = Courses::where('status', '已通過')->get();
 
-        foreach($passed_course as $passed_class)
-        {
-            $class_start_day = strtotime($passed_class->class_start);
-            if($date >= $class_start_day){
-                $passed_class->status = '已開課';
-                $passed_class->save();
-            }
-        }
+        // foreach($passed_course as $passed_class)
+        // {
+        //     $class_start_day = strtotime($passed_class->class_start);
+        //     if($date >= $class_start_day){
+        //         $passed_class->status = '已開課';
+        //         $passed_class->save();
+        //     }
+        // }
 
 
-        $starting_course = Courses::where('status', '已開課')->get();
+        // $starting_course = Courses::where('status', '已開課')->get();
         
-        foreach($starting_course as $starting_class)
-        {
-            $class_end_day = strtotime($starting_class->class_end);
-            if($date >= $class_end_day){
-                $starting_class->status = '已結束';
-                $starting_class->save();
-            }
-        }
+        // foreach($starting_course as $starting_class)
+        // {
+        //     $class_end_day = strtotime($starting_class->class_end);
+        //     if($date >= $class_end_day){
+        //         $starting_class->status = '已結束';
+        //         $starting_class->save();
+        //     }
+        // }
+        dispatch(new CheckCoursesStatus());
     }
 }
