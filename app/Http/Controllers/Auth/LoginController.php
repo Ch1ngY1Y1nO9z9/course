ee19d8f75ea2f7d4ce534c561a7faf9da6ab3312
l123050891 <l123050891@gmail.com>
2021-10-25 13:48:42 +0800

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -60,14 +60,34 @@ class LoginController extends Controller
         $this->incrementLoginAttempts($request);
         // 記錄過多次嘗試登入
 
+
         // 判斷登入
+        
+        // 先判斷是否在白名單內
+        $account_id = $request->email;
+        $user = User::where('account_id', $account_id)->first();
+
+        if($user && $user->role == 'student'){
+            if(!WhiteList::where('student_id', $user->account_id)->first()){
+                return $this->sendFailedLoginResponse($request); 
+            }
+        }
+
+        // 判斷資料庫是否有資料 有則直接登入
+        if($user){
+            Auth::guard()->login($user);
+
+            return $this->authenticated($request, $this->guard()->user())
+            ?: redirect()->intended($this->redirectPath());
+        }
+
+        // 如果沒帳號就需看SSO回應來決定是否須建立帳號
         if ($Account[0] == 0){
             return $this->sendFailedLoginResponse($request);
         }
         
         if ($Account[0] == 1) {
-
-            // 確認資料返回成功後開始處理資料提供給帳號建立使用
+            // 確認SSO有回應且資料庫內沒有帳號時 開始解析資料並建立帳號
             $Account = array_values(array_filter($Account));
             $AccountData = array_slice($Account, 0, array_search($request->email, $Account));
 
@@ -79,29 +99,8 @@ class LoginController extends Controller
                     $email = $data;
                 }      
             }
-            
-            $account_id = $request->email;
 
-            // 上方資料處理完成後 確認此帳號是否在白名單中 若是Admin或Teacher則不檢查
-
-            $user = User::where('account_id', $account_id)->first();
-
-            // 暫時移除登入時的白名單判斷
-            // if($user && $user->role == 'student'){
-            //     if(!WhiteList::where('student_id', $user->account_id)->first()){
-            //         return $this->sendFailedLoginResponse($request); 
-            //     }
-            // }
-            
-            if($user){
-                Auth::guard()->login($user);
-
-                return $this->authenticated($request, $this->guard()->user())
-                ?: redirect()->intended($this->redirectPath());
-            }
-
-            // 以上都沒被執行代表沒有帳號 以下建立帳號並登入
-
+            // 建立帳號
             $user = User::create(['account_id' => $account_id]);
             
             $password = $request->password;
@@ -145,4 +144,5 @@ class LoginController extends Controller
 
         return $reply;
     }
+
 }
